8f336998a020ff0c02277241e66f684e9d07d083
BUKAN HEKER <70200554+fadel1337@users.noreply.github.com>
2023-03-01 20:31:01 +0700

    Update Gallery.js

--- src/Component/Gallery.js
+++ src/Component/Gallery.js
@@ -10,46 +10,46 @@ export default function Gallery() {
     >
       <Headers />
       <div className="min-h-[93vh] pt-24 wrapper-gallery">
-        <div className="w-11/12 h-[500px] m-auto bg-[#DCDCDC] shadow-md hover:shadow-lg sec-wrapper-gallery">
-          <div className="grid grid-rows-6 grid-cols-3 gap-2 p-8 h-full container-gallery">
+        <div className="w-11/12 h-[2000px] m-auto bg-[#DCDCDC] shadow-md hover:shadow-lg sec-wrapper-gallery">
+          <div className="grid grid-rows-8 grid-cols-3 gap-10 p-8 h-full container-gallery">
             <div className="col-span-1 row-span-3 bg-gray-200">
               <img
-                src={"Assets/image-1.png"}
+                src={"Assets/Poster Pendidikan.png"}
                 alt="card"
                 className="h-full w-full object-fill"
               />
             </div>
             <div className="col-span-1 row-span-2">
               <img
-                src={"Assets/image-4.png"}
+                src={"Assets/Poster Tema Himbauan Membuang Sampah Poster.png"}
                 alt="card"
-                className="h-full w-full object-cover"
+                className="h-full w-full object-fill"
               />
             </div>
             <div className="col-span-1 row-span-3">
               <img
-                src={"Assets/image-3.png"}
+                src={"Assets/ayo semangat belajar untuk mencapai masa depan.png"}
                 alt="card"
                 className="h-full w-full object-fill"
               />
             </div>
             <div className="col-span-1 row-span-4">
               <img
-                src={"Assets/image-5.png"}
+                src={"Assets/jagalah kebersihan.png"}
                 alt="card"
                 className="h-full w-full object-fill"
               />
             </div>
             <div className="col-span-1 row-span-3">
               <img
-                src={"Assets/image-2.png"}
+                src={"Assets/20230301_055601.jpg"}
                 alt="card"
                 className="h-full w-full object-fill"
               />
             </div>
             <div className="col-span-1 row-span-3 ">
               <img
-                src={"Assets/image-6.png"}
+                src={"Assets/20230301_075117.jpg"}
                 alt="card"
                 className="h-full w-full object-fill"
               />
