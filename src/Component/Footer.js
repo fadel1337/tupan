604f758844d5fe3364f43f149c77a6eaf8dc2c60
BUKAN HEKER <70200554+fadel1337@users.noreply.github.com>
2023-03-01 07:03:02 +0700

    Update Footer.js

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <div className="bg-[#DCDCDC] text-center py-3 bg-transparent sm:text-sm text-xs">
-      <p className="font-bold">Copyright © 2023 - Develop By Tupan CodeArt</p>
+      <p className="font-bold">Copyright © 2023 - Develop By Fadel Huzein Adlar</p>
     </div>
   );
 }
